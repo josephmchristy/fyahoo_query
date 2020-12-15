6a082b7fc2c60a28f51f0a208e0d8d4b47873010
josephmchristy <josephmchristy@gmail.com>
2020-12-14 23:59:51 -0800

    Get transactions and add to transactions list.

--- ffyahoo.py
+++ ffyahoo.py
@@ -2,6 +2,7 @@
 
 import xml.etree.ElementTree as ET
 import re
+import datetime
 
 from yahoo_oauth import OAuth2
 
@@ -20,28 +21,6 @@ game_league_ids = {
     '2019': ('390', '655835'),
     '2020': ('399', '414682')
 }
-# num_league_years = len(game_league_ids)
-# num_teams = []
-
-# for gl_id in game_league_ids:
-#    g_id, l_id = game_league_ids[gl_id]
-#    req_url = url+"/league/"+g_id+".l."+l_id
-#    r = oauth.session.get(req_url, params={"format": "json"})
-#    league_info = json.loads(r.text)
-#    num_teams.append(league_info['fantasy_content']['league'][0]['num_teams'])
-
-# for num in num_teams:
-#    print('Number of teams: {}'.format(num))
-
-# Get the number of teams
-g_id = game_league_ids['2020'][0]
-l_id = game_league_ids['2020'][1]
-req_url = url + "/league/" + g_id + ".l." + l_id
-r = oauth.session.get(req_url)
-xmlstring = r.text
-xmlstring = re.sub(' xmlns="[^"]+"', '', xmlstring, count=1)
-league_info = ET.fromstring(xmlstring)
-num_teams = league_info[0].find('num_teams').text
 
 # Get the roster for a team
 req_url = url + "/team/" + "399.l.414682.t.2/roster/players"
@@ -55,20 +34,43 @@ for player in root.iter('player'):
     print(player_fullname.text)
 
 # Get the transactions for the league
-req_url = url + "/league/348.l.521087/transactions"
+req_url = url + "/league/399.l.414682/transactions"
 r = oauth.session.get(req_url)
 xmlstring = r.text
 xmlstring = re.sub(' xmlns="[^"]+"', '', xmlstring, count=1)
 root = ET.fromstring(xmlstring)
+transactions_list = []
 for transaction in root.iter('transaction'):
-    transaction_type = transaction.find('type')
-    if transaction_type.text == 'trade':
+    # For all add/drop or trade transactions
+    transaction_type = transaction.find('type').text
+    if transaction_type != 'commish':
+        # Get the transaction date
+        transaction_timestamp = transaction.find('timestamp').text
+        transaction_date = datetime.date.fromtimestamp(int(transaction_timestamp))
+        # For each player in the transaction
         players = transaction.find('players')
         for player in players:
             transaction_data = player.find('transaction_data')
-            source_team_name = transaction_data.find('source_team_name').text
-            destination_team = transaction_data.find('destination_team_name').text
-            print("Source: {}, Destination: {}".format(source_team_name, destination_team))
+            # Get the player's name
             player_name = player.find('name')
-            player_fullname = player_name.find('full')
-            print(player_fullname.text)
+            p_transaction_name = player_name.find('full').text
+            # Get the transaction type (add, drop, or trade)
+            p_transaction_type = transaction_data.find('type').text
+            # If a player is added via trade or add, get the new owner and check for waiver 
+            if p_transaction_type == 'trade' or p_transaction_type == 'add':
+                p_transaction_owner = transaction_data.find('destination_team_name').text[0:5]
+                p_source_type = transaction_data.find('source_type').text
+                if p_source_type == 'waivers':
+                    p_transaction_type = 'waiver'
+            # If a player is dropped, remove the owner
+            else:
+                p_transaction_owner = ''
+            # Add player transaction data to the transactions list
+            p_transaction = {'date': transaction_date, 'type': p_transaction_type, 'owner': p_transaction_owner, 'player': p_transaction_name}
+            transactions_list.append(p_transaction)
+
+counter = 0
+for transaction in transactions_list:
+    counter += 1
+    if transaction['type'] == 'trade':
+            print(transaction)
