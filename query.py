5f492e264d7794880e431aa6fcecb57394cf187b
josephmchristy <josephmchristy@gmail.com>
2021-01-28 23:53:27 -0800

    Refactored code into fyahoo query class.

--- query.py
+++ query.py
@@ -6,43 +6,82 @@ import xmltodict
 
 from yahoo_oauth import OAuth2
 
-oauth = OAuth2(None, None, from_file='private.json')
-
-if not oauth.token_is_valid():
-    oauth.refresh_access_token()
-
-url = "https://fantasysports.yahooapis.com/fantasy/v2"
 game_league_ids = {
     '2020': ('403', '6851')
 }
 
 
-# Query Yahoo API for data specified by url path
-def yahoo_query(url_path):
-
-    # Request Yahoo API for parameters
-    req_url = url + url_path
-    r = oauth.session.get(req_url)
-
-    # Convert xml response into json
-    xmlstring = r.text
-    xmlstring = re.sub(' xmlns="[^"]+"', '', xmlstring, count=1)
-    xmldict = xmltodict.parse(xmlstring)
-    xmldict = xmldict['fantasy_content']['league']  # Remove added headers
-    jsonstring = json.dumps(xmldict, indent=4)
-    return jsonstring
+class FYahooQuery():
 
 
-# Get all transactions for the league
-def get_league_transactions(game_id, league_id):
+    def __init__(self, league_id, game_code="nfl", season="2020", game_id=None):
+        self.league_id = str(league_id)
+        self.game_code = game_code
+        self.season = str(season)
+        self.url = "https://fantasysports.yahooapis.com/fantasy/v2"
 
-    # Construct the url
-    l_id = game_id + ".l." + league_id
-    url_path = "/league/" + l_id + "/transactions"
+        # Set up auth
+        self.oauth = OAuth2(None, None, from_file='private.json')
+        if not self.oauth.token_is_valid():
+            self.oauth.refresh_access_token()
+    
+        # Retrieve the game id if none was passed in
+        if game_id:
+            self.game_id = str(game_id)
+        else:
+            self.game_id = self.get_game_id()
 
-    # Request and return data
-    league_transactions = yahoo_query(url_path)
-    return league_transactions
+        # Construct the league key
+        self.league_key = self.game_id + ".l." + self.league_id
 
 
-print(get_league_transactions("403", "6851"))
+    # Query Yahoo API for requested data specified by url path
+    def fyahoo_query(self, url_path, req_data):
+    
+        # Request Yahoo API for parameters
+        req_url = self.url + url_path
+        r = self.oauth.session.get(req_url)
+    
+        # Convert xml response into json
+        xmlstring = r.text
+        xmlstring = re.sub(' xmlns="[^"]+"', '', xmlstring, count=1)
+        xmldict = xmltodict.parse(xmlstring)
+        xmldict = xmldict['fantasy_content'][req_data]  # Remove added headers
+        jsonstring = json.dumps(xmldict, indent=4)
+        return jsonstring
+    
+
+    # Get game key
+    def get_game_id(self):
+        
+        # Request json data
+        url_path = "/games;game_codes=" + self.game_code + ";seasons=" + self.season
+        fyahoo_data = self.fyahoo_query(url_path, "games")
+        
+        # Extract game id from json data
+        fyahoo_data = json.loads(fyahoo_data)
+        game_id = fyahoo_data["game"]["game_key"]
+        return game_id
+
+
+    # Get all settings for the league
+    def get_league_settings(self):
+        url_path = "/league/" + self.league_key + "/settings"
+        return self.fyahoo_query(url_path, "league")
+    
+    
+    # Get all transactions for the league
+    def get_league_transactions(self):
+        url_path = "/league/" + self.league_key + "/transactions"
+        return self.fyahoo_query(url_path, "league")
+    
+    
+    # Get team statistics by given week
+    def get_team_stats(self, team_id, week="current"):
+        team_key = self.league_key + ".t." + str(team_id)
+        url_path = "/team/" + team_key + "/stats;type=week;week=" + str(week)
+        return self.fyahoo_query(url_path, "team")
+
+
+nhl = FYahooQuery("6851", "nhl")
+print(nhl.get_team_stats(5))
